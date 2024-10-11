a05de36b72a2569b3781a65de0a0aae6819063ff
Jesus Oroya <41575404+djoroya@users.noreply.github.com>
2024-10-11 21:20:46 +0200

    add

---
+++ README.md
@@ -0,0 +1,5 @@
+npm init -y
+
+
+npm install react react-dom --save-peer
+npm install @babel/preset-react @babel/cli @babel/core --save-dev
---
+++ dist/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+const HolaMundo = () => {
+  return /*#__PURE__*/React.createElement("h1", null, "Hola, Mundo!");
+};
+export default HolaMundo;
